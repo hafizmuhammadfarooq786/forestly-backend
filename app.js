adafc34f776cc6b1675fd072e1ca269627401936
Hafiz Muhammad Farooq <farooq@we-over-i.com>
2021-03-21 19:53:01 +0500

    allow cors

--- app.js
+++ app.js
@@ -37,6 +37,14 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
   res.json(err.message);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
 });
 
 routes.setup(app);
